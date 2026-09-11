6e40a90e95600bde34ba227ebfe4a5aded002c65
touboul <touboulelie22@gmail.com>
2026-09-11 18:18:53 +0200

    création de la fonction compare ainsi que lajout de la fonction demander au joueur

--- python/exercices/tp_final_elie_touboul_Mastermind/main.py
+++ python/exercices/tp_final_elie_touboul_Mastermind/main.py
@@ -6,4 +6,24 @@ def generer_combinaison():
         combinaison.append(chiffre)
     return combinaison
 
-print(generer_combinaison())
+#print(generer_combinaison())
+
+def demander_proposition():
+    while True:
+        saisie = input("veuillez entré 4 chiffre seulement ")
+        if len(saisie) == 4  and saisie.isdigit():
+            proposition = [int(combinaison) for combinaison in saisie]
+            return proposition
+        else :
+            print("votre saisie n'est pas valide veuilly reessayer avec une saisie valide")
+#print(demander_proposition())
+
+def comparer(combinaison, proposition):
+    bien_places = 0
+    mal_places = 0
+    for i in range(len(combinaison)):
+        if combinaison[i] == proposition[i]:
+            bien_places  += 1
+        elif proposition[i] in combinaison:
+            mal_places += 1
+            return bien_places, mal_places
