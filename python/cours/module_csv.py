0b1a37cfc8a25b2f45e9d1953c974da849187346
elie.touboul <touboulelie22@gmail.com>
2026-09-15 16:14:29 +0200

    module csv

---
+++ python/cours/module_csv.py
@@ -0,0 +1,12 @@
+import os
+def create_folder(path, name):
+    chemin = os.path.join(path, name)
+    if not os.path.exists(chemin):
+        os.mkdir(chemin)
+    else:
+        print("le chemin existe deja")
+
+
+path = "."
+name = "test"
+create_folder(path, name)
